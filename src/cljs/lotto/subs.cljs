803aecc4451a8651662e7418346c7146fa67be48
Jay Bienvenu <github@bienv.com>
2016-10-15 15:42:58 -0500

    Update subs.

--- src/cljs/lotto/subs.cljs
+++ src/cljs/lotto/subs.cljs
@@ -2,17 +2,34 @@
     (:require-macros [reagent.ratom :refer [reaction]])
     (:require [re-frame.core :as re-frame]))
 
-(re-frame/reg-sub
- :app-state
- (fn [db]
-   db))
+(re-frame/reg-sub :app-state (fn [db] db))
 
-(re-frame/reg-sub
-  :current-player
+(re-frame/reg-sub :current-player
   (fn [db]
-    (get db :current-player)))
+    (get db :current-player)
+  )
+)
 
-(re-frame/reg-sub
-  :jay
+(re-frame/reg-sub :current-player
   (fn [db]
-    "Jay"))
+    (get db :current-player)
+  )
+)
+
+(re-frame/reg-sub :height
+  (fn [db]
+    (get db :height)
+  )
+)
+
+(re-frame/reg-sub :width
+  (fn [db]
+    (get db :width)
+  )
+)
+
+(re-frame/reg-sub :card-at
+  (fn [db [_ row column]]
+    (get-in db [:cards row column])
+  )
+)
